377317baab3a132d6dfcd4d40d7677e4e5a2048a
Phyu Sin Khant Aung <rollyphyusinkhant7290@gmail.com>
2023-12-24 23:37:18 +0630

    finished up navbar

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Terminal } from "lucide-react";
+import { Switch } from "@/components/ui/switch";
+import { useTheme } from "next-themes";
+
+interface NavItem {
+  name: string;
+  href: string;
+}
+
+export default function Navbar({ navData }: { navData: Array<NavItem> }) {
+  const pathname = usePathname();
+  const { setTheme, theme } = useTheme();
+
+  return (
+    <nav className=" w-full flex justify-center py-3 md:text-lg shadow sticky">
+      <Terminal className="w-[25%]" />
+      <div className=" ">
+        <ul className="flex gap-x-10 md:gap-x-24">
+          {navData.map((item: NavItem) => (
+            <li key={item.name}>
+              <Link
+                href={item.href}
+                className={`hover:underline ${
+                  pathname === item.href && "underline"
+                }`}
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="w-[25%] flex justify-end items-center mr-3">
+        <Switch
+          onClick={() => {
+            if (theme === "light") {
+              return setTheme("dark");
+            } else return setTheme("light");
+          }}
+        />
+      </div>
+    </nav>
+  );
+}
